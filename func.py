db9c1fbdae064828e8e97d039cbc1dbd45f0078d
Jeevan Joseph <you@example.com>
2023-03-13 15:54:34 +0000

    added zipkin

--- func.py
+++ func.py
@@ -2,8 +2,11 @@ import io
 import json
 import logging
 import requests
-import py_zipkin
+import time
+from collections import namedtuple
 
+from py_zipkin.zipkin import zipkin_span
+from py_zipkin.encoding import Encoding
 from fdk import response
 
 
@@ -11,7 +14,7 @@ def handler(ctx, data: io.BytesIO=None):
     tracing_context = ctx.TracingContext()
     with zipkin_span(
         service_name=tracing_context.service_name(),
-        span_name="Customer Code",
+        span_name="Function Handler",
         transport_handler=(
             lambda encoded_span: transport_handler(
                 encoded_span, tracing_context
@@ -31,6 +34,7 @@ def handler(ctx, data: io.BytesIO=None):
 
 
         do_work(ctx)
+        do_more_work(ctx)
         return response.Response(
             ctx, response_data=json.dumps(
                 {"message": "Hello {0}".format(name)}),
@@ -48,7 +52,33 @@ def transport_handler(encoded_span, tracing_context):
 def do_work(ctx):
     with zipkin_span(
         service_name=ctx.TracingContext().service_name(),
-        span_name="Get ADB Password from OCI Vault",
+        span_name="Doing Error Prone work",
+        binary_annotations=ctx.TracingContext().annotations()
+    ) as example_span_context:
+        try:
+            logging.getLogger().debug("Doing some complex task")
+            time.sleep(0.15)
+            # throwing an exception to show how to add error messages to spans
+            raise Exception('Request failed')
+        except (Exception, ValueError) as error:
+            example_span_context.update_binary_annotations(
+                {"Error": True, "errorMessage": str(error)}
+            )
+        else:
+            FakeResponse = namedtuple("FakeResponse", "status, message")
+            fakeResponse = FakeResponse(200, "OK")
+            # how to update the span dimensions/annotations
+            example_span_context.update_binary_annotations(
+                {
+                    "responseCode": fakeResponse.status,
+                    "responseMessage": fakeResponse.message
+                }
+            )            
+
+def do_more_work(ctx):
+    with zipkin_span(
+        service_name=ctx.TracingContext().service_name(),
+        span_name="Do more work",
         binary_annotations=ctx.TracingContext().annotations()
     ) as example_span_context:
         try:
